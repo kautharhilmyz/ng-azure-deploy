c045cd5796045e8f52d90b91c84c0c102579f4e8
Mahmud <alkauthar90@gmail.com>
2016-12-11 16:17:15 +0800

    Init

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: 'prod',
+  apiUrl: 'http://shangyilim-inventory.azurewebsites.net/api/',
+  title: 'NihonFood'
+};
---
+++ src/environments/environment.stage.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  env: 'stage',
+  apiUrl: 'http://shangyilim-inventory.azurewebsites.net/api/',
+  title: 'PhonePlc'
+};
